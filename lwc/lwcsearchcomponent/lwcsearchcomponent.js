5ea0e040dc4779e5cd1bc2d898476399d103351a
Vaibhav Nautiyal <87132728+VaibhavNautiyal@users.noreply.github.com>
2023-10-14 12:21:20 +0530

    Update lwcsearchcomponent.js

--- lwc/lwcsearchcomponent/lwcsearchcomponent.js
+++ lwc/lwcsearchcomponent/lwcsearchcomponent.js
@@ -1,14 +1,4 @@
-/**
- * @File Name          : lwcsearchcomponent.js
- * @Description        :
- * @Author             : Amit Singh (SFDCPanther)
- * @Group              :
- * @Last Modified By   : A Singh
- * @Last Modified On   : 6/8/2020, 10:26:37 PM
- * @Modification Log   :
- * Ver       Date            Author      		    Modification
- * 1.0    5/31/2020   Amit Singh (SFDCPanther)     Initial Version
- **/
+
 import { LightningElement, api, track } from "lwc";
 
 export default class Lwcsearchcomponent extends LightningElement {
