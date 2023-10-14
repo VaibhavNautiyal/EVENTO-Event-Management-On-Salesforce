a22a8d1bb776c6ba8b007debcb67ec47af299e5c
Vaibhav Nautiyal <87132728+VaibhavNautiyal@users.noreply.github.com>
2023-10-14 12:20:50 +0530

    Update lwcrecordlist.js

--- lwc/lwcrecordlist/lwcrecordlist.js
+++ lwc/lwcrecordlist/lwcrecordlist.js
@@ -1,14 +1,4 @@
-/**
- * @File Name          : lwcrecordlist.js
- * @Description        :
- * @Author             : Amit Singh (SFDCPanther)
- * @Group              :
- * @Last Modified By   : A Singh
- * @Last Modified On   : 6/7/2020, 12:41:38 PM
- * @Modification Log   :
- * Ver       Date            Author      		    Modification
- * 1.0    5/31/2020   Amit Singh (SFDCPanther)     Initial Version
- **/
+
 import { LightningElement, api } from "lwc";
 
 export default class Lwcrecordlist extends LightningElement {
